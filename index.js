90cd25bfadcbec5c6a50e7745d3b6e5710a68249
Robin Doirin <robindoirin@gmail.com>
2019-10-27 21:38:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const testVar = {}
-const record = [
-  {year: "2018", result: "L"},
-  {year: "2017", result: "W"},
-  {year: "2016", result: "N/A"}
-  //...
-]
+
 function testFunc() {
   return "hi"
 }
